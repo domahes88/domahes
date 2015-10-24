9832e2e342d7cf0d65fa1fc358c5b4731193fac6
domahes88 <domahes@yandex.ru>
2015-10-24 14:45:06 +0300

    001

---
+++ калькулятор.py
@@ -0,0 +1,18 @@
+# domahes44
+t = int(input('Введите первое число '))
+t2 = int(input('Введите второе число '))
+f = input()
+if f == '+':
+    print(t+t2)
+if f == '-':
+    print(t-t2)
+if f == '*':
+    print(t*t2)
+if f == '**':
+    print(t**t2)
+if f == '/':
+    print(t/t2)
+if f == '//':
+    print(t//t2)
+if f == '%':
+    print(t%t2)
